dab66fc122971d1bf6874c3e36382355fdfda897
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2020-04-18 10:29:35 -0300

    Padronização eslint e configuração, e integração ao projeto

--- GoStack/f02/primeiro-projeto-node/src/server.ts
+++ GoStack/f02/primeiro-projeto-node/src/server.ts
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/',(request, response) => {
- return response.json({ message: 'Hello World'});
-})
+app.get('/', (request, response) => response.json({ message: 'Hello World' }));
 
 app.listen(3333, () => {
- console.log('🚀🚀Server started on port 3333!');
+  console.log('🚀🚀Server started on port 3333!');
 });
